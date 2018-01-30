2a2c02496fc50945817b1a955c98d72d3ab99365
Vartan Benohanian <vartanbeno@gmail.com>
2018-01-30 03:12:05 -0500

    add all functions, calc works now

--- script.js
+++ script.js
@@ -3,56 +3,93 @@ $(".button").css({
     "width": misc + "px"
 });
 
-function add() {
-    let args = Array.from(arguments);
-    let sum = 0;
-    for (i in args) {
-        sum += args[i];
+function operate(operator) {
+    array.push(Number(result.value));
+    switch (operator) {
+        case "division":
+            array.push("/");
+            break;
+        case "multiplication":
+            array.push("*");
+            break;
+        case "subtraction":
+            array.push("-");
+            break;
+        case "addition":
+            array.push("+");
+            break;
+        case "result":
+            array.push("=");
+            operation.value = array.join(" ").replace(/[*]/g, "x").replace(/[/]/g, "÷");
+            result.value = eval(eval(array.join("").slice(0, -1)).toFixed(6));
+            array = [];
+        break;
     }
-    return sum;
+    if (operator != "result") {
+        if (array[array.length - 1] == "=") {
+            operation.value = "";
+            array = [];
+        }
+            result.value = "0";
+    }
 }
 
-function subtract() {
-    let args = Array.from(arguments);
-    let subtraction = args[0];
-    for (i = 1; i < args.length; i++) {
-        subtraction -= args[i];
-    }
-    return subtraction;
+function clearField() {
+    array = [];
+    operation.value = "";
+    result.value = "0";
 }
 
-function multiply() {
-    let args = Array.from(arguments);
-    let product = args[0];
-    for (i = 1; i < args.length; i++) {
-        product *= args[i];
-    }
-    return product;
-}
+let operation = document.querySelector("#screen #operation input");
+let result = document.querySelector("#screen #input input");
+let valueBefore, valueAfter;
+let lookForOperators = /[\*+-\/]/;
 
-function divide() {
-    let args = Array.from(arguments);
-    let division = args[0];
-    for (i = 1; i < args.length; i++) {
-        division /= args[i];
-    }
-    return division;
-}
+let clear = document.querySelector("#clear");
+clear.addEventListener("click", clearField);
 
-function operate(operator, x, y) {
-    if (operator == "+") {
-        return add(x, y);
-    }
-    else if (operator == "-") {
-        return subtract(x, y);
-    }
-    else if (operator == "*") {
-        return multiply(x, y);
-    }
-    else if (operate == "/") {
-        return divide(x, y);
-    }
-    else {
-        return "Choose a valid operator.";
-    }
-}
+let changeSign = document.querySelector("#changeSign");
+changeSign.addEventListener("click", () => {
+    result.value *= -1;
+})
+
+let percentage = document.querySelector("#percentage");
+percentage.addEventListener("click", () => {
+    result.value /= 100;
+})
+
+let numbers = document.querySelectorAll("#numbers .button");
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        if (array[array.length - 1] == "=") {
+            operation.value = "";
+            result.value = "";
+        }
+        if (result.value == "0") {
+            if (number.textContent == ".") {
+                result.value += number.textContent;
+            }
+            else {
+                result.value = number.textContent;
+            }
+            return;
+        }
+        if (number.textContent == "." && result.value.includes(".")) {
+            return false;
+        }
+        result.value += number.textContent;
+    })
+})
+
+let array = [];
+
+let operations = document.querySelectorAll("#operators .button");
+operations.forEach(operation => {
+    operation.addEventListener("click", () => { operate(operation.getAttribute("id")) });
+})
+
+window.addEventListener("keydown", (e) => {
+    let key = document.querySelector(`[data-key="${e.keyCode}"]`)
+    if (!key) return;
+    key.click();
+})
